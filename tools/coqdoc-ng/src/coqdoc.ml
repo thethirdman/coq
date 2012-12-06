9a6c12172c3c83cdc36cdb0c7cd689ac225416c5
Yann Régis-Gianas <yann.regis-gianas@pps.jussieu.fr>
2012-12-06 10:28:31 +0100

    * Coqdoc:   Now describe the architecture of the driver.
    * Settings:
      Beautify.

--- tools/coqdoc-ng/src/coqdoc.ml
+++ tools/coqdoc-ng/src/coqdoc.ml
@@ -7,24 +7,54 @@ open Settings
 open Vdoc
 open Coqtop_handle
 
-(* Takes a coqdoc documentation string, returns a Cst.doc tree *)
-let treat_doc str =
-  let lexbuf = from_string str in
-  (Parser.parse_doc lex_doc lexbuf)
+(** Initialization. *)
+let initialize () = 
+  Settings.parse ()
 
-(* Calls the compilation chain when the input file is a .v*)
-let from_v = assert false
+(** The role of the frontend is to translate a set of input documents
+    written in plenty of formats into a common format that we call 
+    Vdoc.
 
-(* Calls the compilation chain when the input file is a .tex*)
-let from_coqtex = assert false
+    A Vdoc is a "glueing" document composed of two things: (i) fragments of
+    documents written the initial input format ; (ii) requests to coqtop.
+*)
+let frontend () = 
+  match Settings.input_type () with
+    | Settings.IVernac -> assert false (* FIXME: Not implemented yet. *)
+    | Settings.ICoqTeX -> assert false (* FIXME: Not implemented yet. *)
+    | Settings.IHTML   -> assert false (* FIXME: Not implemented yet. *)
 
-(** Main function for coqdoc. Parses the arguments, and generates a .html file
- * from the given .v file *)
-let _ =
-  Settings.parse ();
-  let ct = Coqtop.spawn [] in
-  if !(io.i_type) = Vdoc then
-    from_v ()
-  else
-    from_coqtex ()
+(** A Vdoc is not displayed as is because it contains requests to coqtop. 
+    The purpose of this pass is to turn a Vdoc with requests into a Vdoc
+    where all the requests have been replaced by coqtop answers. (If 
+    they exists). 
+
+    These answers are written in a generic format (the subset of the Vdoc
+    format that represents coqtop answers, typically an XML-like document).
+*)
+let resolve_coqtop_interaction inputs = 
+  (** Initialize a communication layer with a coqtop instance. *)
+  let _ct = Coqtop.spawn [] in  
+  (** Resolve every requests from inputs. *)
+  assert false
+
+(** The role of the backend is to produce the final set of documents.
+
+    The nature of the documents depends on the settings. Anyway,
+    roughly speaking this process consists in inserting the fragments
+    of documents coming from the input files (with a possible
+    conversion into the output format if it is different from the
+    input one) and in pretty printing the coqtop answers into the
+    output format. 
+*)
+let backend resolved_inputs = 
+  assert false
+
+(** Coqdoc is a composition of the passes described below. *)
+let coqdoc =
+  initialize ();
+  let inputs          = frontend () in
+  let resolved_inputs = resolve_coqtop_interaction inputs in
+  backend resolved_inputs
+
 
--- tools/coqdoc-ng/src/settings.ml
+++ tools/coqdoc-ng/src/settings.ml
@@ -1,89 +1,219 @@
-(* This is the option which handles the command line interaction *)
+(** These are the options of the standalone coqdoc driver. **)
 
-open Str
+(** FIXME: Please add a uniform way to produce error messages. *)
+let fatal_error msg = 
+  print_string msg;
+  print_newline ();
+  exit 1
 
-(* File type for input files *)
+(** File type for input files. *)
 type frontend_type =
-  Vdoc
-  | Coqtex
+  (** A documented Coq source code. *)
+  | IVernac
+  (** A LaTeX file that includes Coq snippets. *)
+  | ICoqTeX
+  (** An HTML document that includes Coq snippets. *)
+  | IHTML 
+      
+(** The file extension of a backend_type. *)
+let extension_of_frontend_type = function
+  | IVernac -> ".v"
+  | ICoqTeX -> ".tex"
+  | IHTML -> ".html"
 
-(* File type for output files *)
+(** The frontend_type of a file extension. *)
+let frontend_type_of_extension = function
+  | ".v" -> IVernac
+  | ".tex" -> ICoqTeX
+  | ".html" -> IHTML
+  | _ -> assert false
+
+(** Description of frontend type. *)
+let string_of_frontend_type = function
+  | IVernac -> "coq source"
+  | ICoqTeX -> "TeX file with Coq snippets"
+  | IHTML -> "HTML document with Coq snippets"
+
+(** File type for output files. *)
 type backend_type =
-  Html
-  | Latex
-  | PrettyPrint
+  (** A set of HTML documents, that refer to each other and 
+      may refer to external HTML documents. *)
+  | OHTML
+  (** A self-contained LaTeX document. *)
+  | OLaTeX
+  (** A text-based standalone document. *)
+  | OPrettyPrint
 
-(* Handles input and output files for coqdoc, including channels and
- * filetypes *)
-type file_io = {
-  i_file : string       ref;
-  i_chan : in_channel   ref;
-  i_type : frontend_type ref;
+(** The file extension of a backend_type. *)
+let extension_of_backend_type = function
+  | OHTML -> ".html"
+  | OLaTeX -> ".tex"
+  | OPrettyPrint -> ".pp"
 
-  o_file : string       ref;
-  o_chan : out_channel  ref;
-  o_type : backend_type ref; }
+(** Description of backend type. *)
+let string_of_backend_type = function
+  | OHTML -> "HTML documents"
+  | OLaTeX -> "standalone LaTeX document"
+  | OPrettyPrint -> "standalone text document"
 
-(* If the output file is unspecified the default output will stdout.
- *  If no extension is specified on the output file, coqdoc will chose the
- *  one adapted to the output type*)
+(** Documents are transmitted through standard UNIX channels. *)
+type filename = Anonymous | Named of string
 
-let io = { o_file = ref ""; i_file = ref ""; o_chan = ref stdout;
-                i_chan = ref stdin; o_type = ref Html; i_type = ref Vdoc}
+type ('io, 'io_type) document = {
+  mutable document_filename : filename;
+  mutable document_channel  : 'io;
+  mutable document_type     : 'io_type;
+}
+
+(** Description of expected inputs and outputs. *)
+type input  = (in_channel, frontend_type) document
+
+(* FIXME: For the moment, we do not handle multiple output files...  *)
+type output = (out_channel, backend_type) document
+type io = { 
+  mutable input      : input list;
+  mutable input_type : frontend_type;
+  mutable output     : output;
+}
+
+(** ... and their default values. *)
+let default_input = {
+  document_filename = Anonymous;
+  document_channel  = stdin;
+  document_type     = IVernac;
+}
+
+let default_output = {
+  document_filename = Anonymous;
+  document_channel  = stdout;
+  document_type     = OHTML;
+}
+
+(** Global settings. *)
+let io = { 
+  input      = [ default_input ];
+  output     = default_output;
+  input_type = IVernac;
+}
+
+(** Load a document. *)
+let load_input_document fname = try {
+  document_filename = Named fname;
+  document_channel  = open_in fname;
+  document_type     = frontend_type_of_extension (Filename.chop_extension fname)
+} with (Sys_error _) as e ->
+  (* FIXME: Use a standardize way of raising fatal errors. *)
+  raise e
+
+let load_input_document fname = 
+  io.input <- (load_input_document fname) :: io.input
 
 (* FIXME: make a real usage doc_string *)
 let usage = "This is coqdoc ...\n\n" ^
             "Usage: " ^ Sys.argv.(0) ^ " [options] [files]\n"
 
-let print_help = ref false
+let print_help = 
+  ref false
 
 (* Option list for coqdoc *)
-let speclist = [
-  ("-h", Arg.Set print_help, "Print this help and exits");
-  ("-o", Arg.String (fun s -> io.o_file := s), "Specify output file. If unspecified, default output will be stdout");
-  ("--html", Arg.Unit (fun () -> io.o_type := Html), "produce a html document (default)");
-  ("--latex", Arg.Unit (fun () -> io.o_type := Latex), "produce a Latex document");
-  ("--pp", Arg.Unit (fun () -> io.o_type := PrettyPrint), "produce a Latex document");
+(* FIXME: This should include at least all the options of the old coqdoc
+   FIXME: as well as all the options of old coqtex. 
+   FIXME: In addition, we would like an emulation mode for the old coqdoc
+   FIXME: and also a wrapping of the old implementation.
+*)
+let speclist = Arg.align [
+  ("-h", Arg.Set print_help, 
+   " Print this help message and exit.");
+
+  ("-o", Arg.String (fun s -> io.output.document_filename <- Named s), 
+   " Specify output file. If unspecified, default output will be stdout.");
+
+  ("--html", Arg.Unit (fun () -> io.output.document_type <- OHTML), 
+   " Produce a HTML document (default).");
+
+  ("--latex", Arg.Unit (fun () -> io.output.document_type <- OLaTeX), 
+   " Produce a LaTeX document.");
+
+  ("--pp", Arg.Unit (fun () -> io.output.document_type <- OPrettyPrint), 
+   " Produce a LaTeX document.");
   ]
 
-(* Function for parsing anonymous arguments *)
+let print_help_if_required () = 
+  if !print_help then (
+    print_string (Arg.usage_string speclist usage); 
+    exit 0
+  )
+
+(** Load requested inputs. *)
 let parse_anon = function
-    s when Sys.file_exists s -> io.i_file := s;
-    | x -> raise (Arg.Bad ("Invalid argument: " ^ x))
+  | s when Sys.file_exists s -> 
+    load_input_document s
+  | x -> 
+    raise (Arg.Bad ("Invalid argument: " ^ x))
 
-(* Obtains the extension of a file from a path. Returns None if there is no
- * extension in the path, Some "ext" otherwise
- *)
-let get_ext str =
-    let reg = Str.regexp "\\." in
-  try
-    let off = Str.search_backward reg str (String.length str) in
-    Some (Str.string_after str off)
-  with Not_found -> None
+(** Check settings consistency. *)
+let check_settings_consistency () = 
+  let rec check () = 
+    check_output_extension_consistency ();
+    check_feasible_translation ()
 
-(* Generates a file exntesion from a backend_type *)
-let gen_ext prefix = function
-  Html ->          (prefix ^ ".html")
-  | Latex ->       (prefix ^ ".tex")
-  | PrettyPrint -> (prefix ^ ".pp")
+  (** Check output extension: the output document type and filename
+      extension must coincide. *)
+  and check_output_extension_consistency () = 
+    match io.output.document_filename with
+      | Anonymous -> 
+	(** If no filename is specified, we are good. *)
+	()
+      | Named filename -> 
+	let extension = Filename.chop_extension filename in
+	let xextension = extension_of_backend_type io.output.document_type in
+	if extension <> xextension then
+	  fatal_error (Printf.sprintf 
+			 "The %s back-end only creates `%s' files."
+			 (string_of_backend_type io.output.document_type)
+			 xextension)
 
-(* Takes a file extension and returns the associated frontend_type *)
-let set_entry_type = function
-  Some ".tex" -> Coqtex
-  | _ -> Vdoc
+  (** They are a couple of front-end/back-end that are not implemented. *)
+  and check_feasible_translation () = 
+    let rec similar_inputs p is = 
+      match p, is with
+	| None, [] -> 
+	  fatal_error "I cannot produce a document out of nothing!"
+	| None, i :: is ->
+	  similar_inputs (Some i.document_type) is
+	| Some t, [] ->
+	  t
+	| Some t, i :: is -> 
+	  if i.document_type <> t then 
+	    fatal_error "I need all the input files to be of the same kind."
+	  else 
+	    similar_inputs p is
+    in
+    let input_type = similar_inputs None io.input in
+    io.input_type <- input_type;
+    match input_type, io.output.document_type with
+      | IHTML, (OLaTeX | OPrettyPrint) ->
+	fatal_error (Printf.sprintf 
+		       "You cannot produce a %s document out of a %s input."
+		       (string_of_backend_type io.output.document_type)
+		       (string_of_frontend_type input_type))
+      | _ -> 
+	()
+  in
+  check ()
 
-(* Parses the command line and sets up the variables *)
+(** Parses the command line and sets up the variables. *)
 let parse () =
   Arg.parse speclist parse_anon usage;
-  if (!print_help) then
-    (print_string (Arg.usage_string speclist usage); exit 0)
-  else
-    io.i_chan := open_in !(io.i_file);
-    if !(io.o_file) <> "" then begin
-      let ext = get_ext !(io.o_file) in
-      if ext = None then begin
-        io.o_file := gen_ext !(io.o_file) !(io.o_type)
-      end;
-      io.o_chan := open_out !(io.o_file)
-    end;
-    io.i_type := set_entry_type (get_ext !(io.i_file))
+  print_help_if_required ();
+  check_settings_consistency ()
+
+(** Accessors *)
+let input_documents () = io.input
+let input_filename i   = i.document_filename 
+let input_channel i    = i.document_channel
+let input_type ()      = io.input_type
+let output_document () = io.output
+let output_filename o  = o.document_filename
+let output_channel o   = o.document_channel
+let output_type o      = o.document_type
